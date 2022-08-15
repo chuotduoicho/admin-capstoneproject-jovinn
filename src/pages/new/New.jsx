03b31e60d28ba74349bc1ec44432499a5c492980
duc24600 <92635961+duc24600@users.noreply.github.com>
2022-08-15 14:39:39 +0700

    fixed skillTable actions

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,12 +1,19 @@
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const New = ({ inputs, title }) => {
-  const [file, setFile] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const handleAdd = (e) => {
+    e.preventDefault();
 
+
+  }
   return (
     <div className="new">
       <Sidebar />
@@ -17,29 +24,9 @@ const New = ({ inputs, title }) => {
         </div>
         <div className="bottom">
           <div className="left">
-            {/* <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt=""
-            /> */}
           </div>
           <div className="right">
             <form>
-              {/* <div className="formInput">
-                <label htmlFor="file">
-                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
-                </label>
-                <input
-                  type="file"
-                  id="file"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  style={{ display: "none" }}
-                />
-              </div> */}
-
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
